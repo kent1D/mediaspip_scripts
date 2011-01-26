2a039971faf5e18d1acc59cb274dd7917e9634fd
kent1 <kent1@arscenic.info>
2011-01-26 00:53:43 +0000

    oups

--- distribs/centos_5_3_common.sh
+++ distribs/centos_5_3_common.sh
@@ -571,7 +571,7 @@ centos_5_3_ffmpeg_php_install ()
 	echo $(eval_gettext "Info compilation make")
 	make -j $NO_OF_CPUCORES 2>> $LOG >> $LOG || return 1
 	make install 2>> $LOG >> $LOG || return 1
-	echo 'extension=ffmpeg.so' > /etc/php.d/ 2>> $LOG >> $LOG || return 1
+	echo 'extension=ffmpeg.so' > /etc/php.d/ffmpeg.ini 2>> $LOG >> $LOG || return 1
 	service httpd graceful 2>> $LOG >> $LOG || return 1
 	REVISION=$(env LANG=C svn info --non-interactive | awk '/^Revision:/ { print $2 }')
 	echo
