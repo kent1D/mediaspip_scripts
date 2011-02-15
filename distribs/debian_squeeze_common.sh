43ef45d21014e8a43fcf927a9fd5bd9a6fc342b2
kent1 <kent1@arscenic.info>
2011-02-15 14:52:22 +0000

    On met le file_upload à On

--- distribs/debian_squeeze_common.sh
+++ distribs/debian_squeeze_common.sh
@@ -349,7 +349,8 @@ debian_squeeze_apache_install ()
 	echo
 	
 	echo $(eval_gettext "Info php max_upload $PHP_UPLOAD_SIZE")
-	echo "upload_max_filesize = $PHP_UPLOAD_SIZE" > /etc/php5/conf.d/mediaspip_upload.ini
+	echo "file_uploads = On" > /etc/php5/conf.d/mediaspip_upload.ini
+	echo "upload_max_filesize = $PHP_UPLOAD_SIZE" >> /etc/php5/conf.d/mediaspip_upload.ini
 	echo "post_max_size = $PHP_UPLOAD_SIZE" >> /etc/php5/conf.d/mediaspip_upload.ini
 	echo
 	
