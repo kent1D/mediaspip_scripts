69e7d0267edc465f63a1600bbe70c2025e35b213
kent1 <kent1@arscenic.info>
2012-07-24 11:49:33 +0000

    On nécessite unzip donc on l'installe

--- distribs/debian_lenny_common.sh
+++ distribs/debian_lenny_common.sh
@@ -396,7 +396,7 @@ debian_lenny_dep_install()
 	echo $(eval_gettext "Info apt maj paquets")
 	echo $(eval_gettext "Info apt maj paquets") 2>> $LOG >> $LOG
 	apt-get -y --force-yes remove php5-imagick 2>> $LOG >> $LOG || return 1
-	apt-get -y --force-yes install build-essential subversion git-core checkinstall libcxxtools-dev scons libboost-dev zlib1g-dev \
+	apt-get -y --force-yes install build-essential subversion git-core checkinstall libcxxtools-dev scons libboost-dev zlib1g-dev unzip \
 		apache2.2-common mysql-server php5-dev php5-mysql php-pear php5-curl php5-gd libmagick9-dev ruby yasm texi2html \
 		libfaac-dev libfaad-dev libdirac-dev libgsm1-dev libopenjpeg-dev libxvidcore4-dev libschroedinger-dev libspeex-dev libvorbis-dev \
 		flac vorbis-tools xpdf poppler-utils catdoc \
