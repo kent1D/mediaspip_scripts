d1b9f9571e2c3271817c1aca62f1d8113305e02a
kent1 <kent1@arscenic.info>
2013-02-07 23:09:25 +0000

    On simplifie les mes_options et on le fait fonctionner si pas en mode ferme (à ce moment là, juste $quota_cache de différent)

--- configs/spip/mes_options.php
+++ configs/spip/mes_options.php
@@ -1,21 +1,32 @@
 <?php
 	
-	if (!defined("_ECRIRE_INC_VERSION")) return;
+if (!defined("_ECRIRE_INC_VERSION")) return;
 	
-	/*
+/**
+ * Autoriser un cache de 50Mo
+ */
+$quota_cache = 50;
+
+/**
+ * Est on dans le cas d'une mutualisation
+ * Si le répertoire mutualisation est là c'est que c'est bon
+ */
+if (file_exists (_DIR_RACINE.'mutualisation/mutualiser.php')) {
+	if (file_exists (_DIR_RACINE.'mes_options_personnalisation.php'))
+		require 'mes_options_personnalisation.php';
+	
+	require _DIR_RACINE.'mutualisation/mutualiser.php';
+	
+	/**
 	 * Inscrire ici le nom du site d'administration du tableau de bord
 	 * de la mutualisation (ou plusieurs, separes par des virgules)
 	 * pour autoriser tous les sites, ne pas definir la constante ;
 	 * Si le site maitre n'est pas dans sites/ mais a la racine, mettre ''
 	 * et ajouter 'mutualisation' dans $dossier_squelettes
 	 */
-	define ('_SITES_ADMIN_MUTUALISATION', 'www.mondomaine.org');
+	define ('_SITES_ADMIN_MUTUALISATION', $site_mutualisation);
 	
-	if (!is_readable (_DIR_RACINE.'mutualisation/mutualiser.php')) {
-		echo _L("Fichier 'mutualisation/mutualiser.php' manquant dans la racine " . _DIR_RACINE);
-		exit;
-	}
-	require _DIR_RACINE.'mutualisation/mutualiser.php';
+	
 
 	$site = $_SERVER['HTTP_HOST'];
 
@@ -31,22 +42,21 @@
 	}
 	
 	/**
-	 * Autoriser un cache de 50Mo
-	 */
-	$quota_cache = 50;
-	
-	/**
 	 * Ne pas autoriser les plugins auto
 	 */
 	define ('_DIR_PLUGINS_AUTO', false);
 	define('_AUTORISER_TELECHARGER_PLUGINS',false);
 	
-	define ('_INSTALL_SITE_PREF', prefixe_mutualisation($site));
-	define ('_INSTALL_SERVER_DB', 'mysql');
-	define ('_INSTALL_HOST_DB', 'localhost');
-	define ('_INSTALL_USER_DB', 'user');
-	define ('_INSTALL_PASS_DB', 'pass');
-	define ('_INSTALL_NAME_DB', 'mu_'._INSTALL_SITE_PREF);
+	if(isset($db_user) && isset($db_pass)){
+		define ('_INSTALL_SITE_PREF', prefixe_mutualisation($site));
+		define ('_INSTALL_SERVER_DB', 'mysql');
+		define ('_INSTALL_HOST_DB', 'localhost');
+		define ('_INSTALL_USER_DB_ROOT', $db_user);
+		define ('_INSTALL_PASS_DB_ROOT', $db_pass);
+		define ('_INSTALL_NAME_DB', 'mu_'._INSTALL_SITE_PREF);
+	}else{
+		echo _L('User et pass de base de donnée non fournis');
+	}
 
 	/**
 	 * Mettre en commentaire la ligne suivante si vous utilisez l'option table_prefixe plus bas dans la config 
@@ -81,14 +91,6 @@
 	 * define ('_INSTALL_PASS_DB_ROOT', 'pass_root');
 	 */
 
-	/*
-	 * Creer les bases de donnees via un ping sur une URL (methode AlternC)
-	 *
-	 * Il suffit de renseigner l'option url_creer_base, en lui passant les bons parametres :
-	 *
-	 * 'url_creer_base' => 'http://localhost/admin/sql_doadd.php?username=user&password=pass_user&dbn='.prefixe_mutualisation($site)
-	 */
-
 
 	/*
 	 * Transformer sur les pages publiques les url des images
@@ -108,26 +110,29 @@
 	 * grace a ?var_mode=creer_htaccess_img
 	 *
 	 */
-
-	demarrer_site($site,
-		array(
+	$conf_site = array(
 			'creer_site' => true,        	// Creer ou non le site s'il n'existe pas (defaut: false)
 			'creer_base' => true,        	// Creer ou non la base de donnee si elle n'existe pas (false)
-			'creer_user_base' => false,  	// Creer ou non un utilisateur pour la nouvelle base de donnee (false)
+			'creer_user_base' => true,  	// Creer ou non un utilisateur pour la nouvelle base de donnee (false)
 			'mail' => 'email@mondomaine.org', // Adresse mail pour recevoir un mail lors d'une creation de site mutualise ('')
 			'code' => 'pass_mutu',			// Code d'activation de base
 			'table_prefix' => false,		// Definir automatiquement le prefixe de table (false) ... mettre true si tous les sites dans la meme base
 			'cookie_prefix' => true,		// Definir automatiquement le prefixe de cookie (false)
 			'repertoire' => 'sites',		// Nom du repertoire contenant les sites mutualises ('sites')
-			'url_img_courtes' => true,		// Utiliser la redirection des URL d'images courtes dans la partie publique (false)
+			//'url_img_courtes' => true,		// Utiliser la redirection des URL d'images courtes dans la partie publique (false)
 											// /!\ il faut qu'apache ait le droit d'ecrire dans les dossiers IMG/ et local/ a la racine du site !
 											// C'est la que la mutualisation va ecrire les regles de redirection automatiques pour les images de chaque site
 			'utiliser_panel' => true, // Utiliser une table externe pour recuperer des identifiants ... (code, user, pass) permettant a un utilisateur d'installer le site (false)
 			'annonce' => '', // Texte a afficher en bas du formulaire d'activation de la mutualisation
-			//'url_creer_base' => 'http://localhost/admin/sql_doadd.php?username=user&password=pass_user&dbn='.prefixe_mutualisation($site), // Creer la base de donnees via une URL (methode AlternC)
-			'url_hebergeur' => 'http://'._SITES_ADMIN_MUTUALISATION,
-			'url_contact_hebergeur' => 'http://'._SITES_ADMIN_MUTUALISATION.'/spip.php?page=contact'
-		)
-	);
+		);
+	if(defined(_SITES_ADMIN_MUTUALISATION) && strlen(_SITES_ADMIN_MUTUALISATION) > 1){
+		$conf_site['url_hebergeur'] = 'http://'._SITES_ADMIN_MUTUALISATION;
+		$conf_site['url_contact_hebergeur'] = 'http://'._SITES_ADMIN_MUTUALISATION.'/spip.php?page=contact';
+	}
+	
+	demarrer_site($site,
+		$conf_site
+	);	
+}
 
 ?>
